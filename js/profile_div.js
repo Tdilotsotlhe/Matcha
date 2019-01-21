7f7f3d998e0ee9f0accdfa07763eb5c2255e880b
Tshiamo DILOTSOTLHE <tdilotso@e4r10p2.wethinkcode.co.za>
2019-01-21 16:20:46 +0200

    profile only please...

--- js/profile_div.js
+++ js/profile_div.js
@@ -25,7 +25,7 @@ return $.post('functions/userfunctions.php?action=getpics', function (response) 
 }
 
 function loadMembers(){
-  alert("member loader");
+  //alert("member loader");
   $.ajaxSetup({async: false});  
  members = $.post('functions/userfunctions.php?action=loadmembers', function (response) {
  });
@@ -39,6 +39,36 @@ function loadMembers(){
    //make div
    console.log(memberArray[i][1]);
    var carddiv = $("<div class='w3-card-4'></div>");
+
+
+
+
+
+imageCard = '<div class="w3-container w3-card w3-white w3-round w3-margin"><br>\
+<img src="/w3images/avatar2.png" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">\
+<span class="w3-right w3-opacity">1 min</span>\
+		<h4>John Doe</h4><br>\
+		<hr class="w3-clear">\
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut\
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\
+			laboris nisi ut aliquip ex ea commodo consequat.</p>\
+		<div class="w3-row-padding" style="margin:0 -16px">\
+			<div class="w3-half">\
+				<img src="/w3images/lights.jpg" style="width:100%" alt="Northern Lights" class="w3-margin-bottom">\
+			</div>\
+			<div class="w3-half">\
+				<img src="/w3images/nature.jpg" style="width:100%" alt="Nature" class="w3-margin-bottom">\
+			</div>\
+		</div>\
+		<button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"><i class="fa fa-thumbs-up"></i>\
+			 Like</button>\
+		<button type="button" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fa fa-comment"></i>\
+			 Comment</button>\
+	</div>';
+
+
+
+
    var ptag = $('<p></p>').text(memberArray[i]['username']); 
 
    ptag.appendTo(carddiv);
@@ -68,13 +98,19 @@ function loadGallery(){
     //alert(images[p]);
     num = Math.floor((Math.random() * 100) + 1);
      var img = $('<img>'); 
+     img.attr('id', images[p]);
     img.attr('src', "images/"+images[p]+"?v="+num);
     img.attr('width', "60px");
     img.attr('height', "60px");
     img.attr('name', images[p]);
     img.appendTo('#profilePics');
     img.on('click', function() {
-      alert(this.name);
+    var r = confirm("Set as profile picture?");
+    if (r == true) {
+      setProfPic(this.id);
+    } else {
+      txt = "You pressed Cancel!";
+    }
     });
     //$('#profilePics').append(images.responseText[p]);
 
@@ -83,6 +119,34 @@ function loadGallery(){
  // $('#profilePics').html(JSON.parse(images.responseText));
 }
 
+function setProfPic(imagname) {
+  $.post('functions/userfunctions.php?action=setPic&imgname='+imagname, function (response) {
+    //console.log(response);
+     newDP = loadProfPic().responseText;
+     //console.log(newDP);
+     newDP = JSON.parse(newDP);
+     newDP = JSON.parse(newDP['profile']);
+     console.log(newDP['dpset']);
+     $("#profPic").attr("src", "images/"+newDP['dpset']+"?sdfg43");
+    //  newDP['dpset'];
+     //console.log(JSON.parse(newDP.responseText));
+     //picName = JSON.parse(newDP['profile']);
+     //console.log(picName);
+    
+  });
+  ///loadprofilepicfunction
+}
+
+function loadProfPic(){
+ return $.post('functions/userfunctions.php?action=dpLoad', function (response) {
+    //console.log(response);
+   /*  wholeRow = JSON.parse(response);
+    wholeRow2 = JSON.parse(wholeRow['profile']);
+    console.log(wholeRow2['dpset']);
+    $("profPic").attr("src", "images/"+wholeRow2['dpset']); */
+
+  });
+}
 
 function fetchUserinfo(){
    //return allAjax("functions/userfunctions.php","post","loadProfile=1","false","getProf");
@@ -111,20 +175,31 @@ function ajaxupload() {
    }
    hr.send(formData); 
 
- 
   }
+
+function fetchAndLoadDP(){
+  theuser = fetchUserinfo();
+  theuser = JSON.parse(theuser.responseText);
+  theuser = JSON.parse(theuser['profile']);
+  $("#profPic").attr("src", "images/"+theuser['dpset']);
+
+}
 
 
 
 function loadProfile(userprof) {
 
-//console.log(userprof);
-//alert(userprof);
+fetchAndLoadDP();
+
+
+////////////
+
 $("#profileName").html(userprof['first_name']);
 
 theprof = JSON.parse(userprof['profile']);
 //alert(theprof.location);
 theprof2 = JSON.parse(theprof.interests);
+console.log(theprof2);
 
 $("#interests").html("");
 for(var w = 0; w < theprof2.length; w++){
